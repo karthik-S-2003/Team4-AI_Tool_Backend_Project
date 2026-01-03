0d066a68f47a6132895fe255c431afcf5e66390f
karthik-S-2003 <Karthik.S@ust.trumio.ai>
2026-01-03 18:17:43 +0530

    Update main.py

--- app/main.py
+++ app/main.py
@@ -5,6 +5,9 @@ from app.admin.routes import router as admin_router
 from app.user.routes import router as user_router
 from app.models import Admin
 from app.auth import get_password_hash
+from fastapi.middleware.cors import CORSMiddleware
+
+
 
 
 @asynccontextmanager
@@ -43,3 +46,11 @@ Base.metadata.create_all(bind=engine)
 @app.get("/")
 def greet():
     return "Welcome to Backend AiTools Project !!!"
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
